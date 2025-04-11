e66fc8e9b7e3e46c91ac0baf63405d1a9c5b3f96
gustavomurar <gustavomurarosoares3@gmail.com>
2025-04-11 14:44:03 -0300

    feat: criação da API para Autor.

--- app/urls.py
+++ app/urls.py
@@ -7,14 +7,14 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 
-from core.views import CategoriaViewSet, EditoraViewSet, UserViewSet
+from core.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, AutorViewSet, UserViewSet
 
 router = DefaultRouter()
 
 router.register(r'categorias', CategoriaViewSet, basename='categorias')
 router.register(r'usuarios', UserViewSet, basename='usuarios')
-router.register(r"categorias", CategoriaViewSet)
-router.register(r"editoras", EditoraViewSet)
+router.register(r"editoras", EditoraViewSet, basename='editoras')
+router.register(r"autores", AutorViewSet, basename='autores')
 
 
 urlpatterns = [
