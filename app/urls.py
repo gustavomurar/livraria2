3ea0d0cf1e46bb217fe01d993711b7d2fb33c7b6
gustavomurar <gustavomurarosoares3@gmail.com>
2025-05-20 09:28:51 -0300

    feat: criação da model de uploud de imagem.

--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (
@@ -7,8 +9,9 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 
-from core.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, AutorViewSet, LivroViewSet, UserViewSet
 
+from core.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, AutorViewSet, LivroViewSet, UserViewSet
+from uploader.router import router as uploader_router
 router = DefaultRouter()
 
 router.register(r'categorias', CategoriaViewSet, basename='categorias')
@@ -32,6 +35,9 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name='schema'),
         name='redoc',
     ),
+    path("api/media/", include(uploader_router.urls)),  
     # API
     path('api/', include(router.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
