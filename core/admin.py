961d42d1033adcaeae51580f54eb97b5cd99ec61
gustavomurar <gustavomurarosoares3@gmail.com>
2025-07-01 08:43:53 -0300

    feat: criação da entidade Compra integrada ao usuário do projeto.

--- core/admin.py
+++ core/admin.py
@@ -6,7 +6,55 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from core.models import Autor, Categoria, Editora, Livro, User
+from core.models import Autor, Categoria, Compra, Editora, Livro, User
+
+
+#admin.site.register(models.User, UserAdmin)
+#admin.site.register(models.Categoria)
+#admin.site.register(models.Editora)
+#admin.site.register(models.Autor)
+#admin.site.register(models.Livro)
+@admin.register(Compra)
+class CompraAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'status')
+    search_fields = ('usuario__email', 'usuario__name')
+    list_filter = ('status',)
+    ordering = ('usuario', 'status')
+    list_per_page = 10
+
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email')
+    search_fields = ('nome', 'email')
+    list_filter = ('nome',)
+    ordering = ('nome', 'email')
+    list_per_page = 10
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('descricao',)
+    search_fields = ('descricao',)
+    list_filter = ('descricao',)
+    ordering = ('descricao',)
+    list_per_page = 10
+
+@admin.register(Editora)
+class EditoraAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'cidade')
+    search_fields = ('nome', 'email', 'cidade')
+    list_filter = ('nome', 'email', 'cidade')
+    ordering = ('nome', 'email', 'cidade')
+    list_per_page = 10
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'editora', 'categoria')
+    search_fields = ('titulo', 'editora__nome', 'categoria__descricao')
+    list_filter = ('editora', 'categoria')
+    ordering = ('titulo', 'editora', 'categoria')
+    list_per_page = 25
+
+
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
@@ -49,43 +97,3 @@ class UserAdmin(BaseUserAdmin):
             },
         ),
     )
-
-
-#admin.site.register(models.User, UserAdmin)
-#admin.site.register(models.Categoria)
-#admin.site.register(models.Editora)
-#admin.site.register(models.Autor)
-#admin.site.register(models.Livro)
-
-
-@admin.register(Autor)
-class AutorAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'email')
-    search_fields = ('nome', 'email')
-    list_filter = ('nome',)
-    ordering = ('nome', 'email')
-    list_per_page = 10
-
-@admin.register(Categoria)
-class CategoriaAdmin(admin.ModelAdmin):
-    list_display = ('descricao',)
-    search_fields = ('descricao',)
-    list_filter = ('descricao',)
-    ordering = ('descricao',)
-    list_per_page = 10
-
-@admin.register(Editora)
-class EditoraAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'email', 'cidade')
-    search_fields = ('nome', 'email', 'cidade')
-    list_filter = ('nome', 'email', 'cidade')
-    ordering = ('nome', 'email', 'cidade')
-    list_per_page = 10
-
-@admin.register(Livro)
-class LivroAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'editora', 'categoria')
-    search_fields = ('titulo', 'editora__nome', 'categoria__descricao')
-    list_filter = ('editora', 'categoria')
-    ordering = ('titulo', 'editora', 'categoria')
-    list_per_page = 25
